de34e5ce719c1936cdfec4d74a52b6d5feed4aa9
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-04-30 21:58:17 +0600

    added typescript learning

---
+++ src/primitive.ts
@@ -0,0 +1,15 @@
+//? Js - Ts
+// String, Number,boolean,undefined,null
+
+//?Ts : never,unknown,void
+
+let userName:String = "Shahadat hasan"
+
+let userId = 123
+
+let isAdmin:boolean =true 
+isAdmin = false
+
+let x:undefined = undefined ;
+
+
---
+++ src/test.ts
@@ -0,0 +1,2 @@
+const course :String ="next Level web development"
+console.log(course);
