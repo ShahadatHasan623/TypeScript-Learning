b233398a50812bdcfd4c46f42f18ee7bb0efe26d
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-05-01 17:44:42 +0600

    added ternary operator and nullish operator

---
+++ src/questionMark.ts
@@ -0,0 +1,32 @@
+// ?:Ternary operator ---> decision making
+// ?? : nullish coalescing ---> null/undefined
+// ?. optional chaining
+
+const biyerJonnoEligible = (age: number) => {
+  // if(age >= 21){
+  //     console.log('you are eligible');
+  // }
+  // else{
+  //     console.log('you are a not eligible !');
+  // }
+
+  const result = age >= 21 ? "you are eligible" : "you are a not eligible !";
+  console.log(result);
+};
+
+biyerJonnoEligible(20);
+
+
+// ?? nullish operator
+
+const userTheme =null;
+const selectedTheme = userTheme ?? 'light theme'
+console.log(selectedTheme);
+
+const isAuthenticated ='';
+
+const ternaryAuthenticated = isAuthenticated ? isAuthenticated : "You are guest !"
+
+const nullishAuthenticated = isAuthenticated ?? "You are guest"
+
+console.log({ternaryAuthenticated},{nullishAuthenticated});
