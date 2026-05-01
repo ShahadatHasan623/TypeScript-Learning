c541d4014d669a929c1c98a119a85f60a42d1b27
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-05-01 17:50:51 +0600

    added optional chaining

--- src/questionMark.ts
+++ src/questionMark.ts
@@ -16,17 +16,47 @@ const biyerJonnoEligible = (age: number) => {
 
 biyerJonnoEligible(20);
 
-
 // ?? nullish operator
 
-const userTheme =null;
-const selectedTheme = userTheme ?? 'light theme'
+const userTheme = null;
+const selectedTheme = userTheme ?? "light theme";
 console.log(selectedTheme);
 
-const isAuthenticated ='';
+const isAuthenticated = "";
 
-const ternaryAuthenticated = isAuthenticated ? isAuthenticated : "You are guest !"
+const ternaryAuthenticated = isAuthenticated
+  ? isAuthenticated
+  : "You are guest !";
 
-const nullishAuthenticated = isAuthenticated ?? "You are guest"
+const nullishAuthenticated = isAuthenticated ?? "You are guest";
 
-console.log({ternaryAuthenticated},{nullishAuthenticated});
+console.log({ ternaryAuthenticated }, { nullishAuthenticated });
+
+//optional chaining ----> ?
+
+const userData: {
+  age: number;
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+  address: {
+    division: string;
+    city: string;
+  },
+  contactNo:string
+} = {
+  age: 20,
+  name: {
+    firstName: "shahadat",
+    lastName: "hossain",
+  },
+  address: {
+    division: "Barisal",
+    city: "Bhola",
+  },
+  contactNo: "017",
+};
+
+const user =userData?.address?.village
+console.log(user);
